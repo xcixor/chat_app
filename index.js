693c10c75ebb4fa99a537c7c2f50c8612aa2f48e
xcixor <pndungu54@gmail.com>
2022-09-20 19:58:58 +0300

    chore(environment): setup dev environment

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./public/style.css";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
+
+function Test() {
+	return <h1>Hello World!</h1>;
+}
+
+root.render(
+	<React.StrictMode>
+			<Test />
+	</React.StrictMode>
+);
