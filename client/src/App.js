338827dccf0f489acf63db78f9424ac4b8136a10
xcixor <pndungu54@gmail.com>
2022-09-21 04:50:04 +0300

    chore(storage): add a local storage hook to persist data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import Login from "./features/auth/components/Login";
+import useLocalStorage from "./features/auth/hooks/useLocalStorage"
 
 function App() {
-	const [name, setName] = useState("id");
+	const [name, setName] = useLocalStorage("id");
 	return <Login onNameSubmit={setName} />;
 }
 
