29dbe4f23f30fe1c2d22d0eca6ed62692b51cb72
xcixor <pndungu54@gmail.com>
2022-09-22 04:21:15 +0300

    feat(navigation): add navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import React, { useState } from "react";
 import { Login } from "./features";
 import useLocalStorage from "./features/auth/hooks/useLocalStorage";
 import Dashboard from "./pages/Dashboard";
+import Navbar from "./layouts/Navbar";
 
 function App() {
 	const [id, setId] = useLocalStorage("id");
-	return id ? <Dashboard id={id} /> : <Login onNameSubmit={setId} />;
+	return id ? (
+		<>
+			<Navbar currentUser={id} />
+			<Dashboard id={id} />
+		</>
+	) : (
+		<Login onNameSubmit={setId} />
+	);
 }
 
 export default App;
