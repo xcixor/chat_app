61d19bc13b9e5323120aab657fa2eac799218de0
xcixor <pndungu54@gmail.com>
2022-09-26 12:45:32 +0300

    chore(app): configure router in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import { Login } from "./features";
 import useLocalStorage from "./hooks/useLocalStorage";
 import Dashboard from "./pages/Dashboard";
@@ -11,27 +11,32 @@ import PageNotFound from "./pages/PageNotFound";
 function App() {
 	const [id, setId] = useLocalStorage("id");
 	const [messages, setMessages] = useLocalStorage("messages", []);
-	const items = [];
 	return id ? (
 		<>
 			<Navbar currentUser={id} />
-			<Routes>
-				<Route
-					exact
-					path="/"
-					element={
-						<Dashboard id={id} messages={messages} setMessages={setMessages} />
-					}
-				/>
-				<Route
-					exact
-					path="/history"
-					element={
-						<History currentUser={id} items={messages} itemsPerPage={25} />
-					}
-				/>
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
+			<Router>
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<Dashboard
+								id={id}
+								messages={messages}
+								setMessages={setMessages}
+							/>
+						}
+					/>
+					<Route
+						exact
+						path="/history"
+						element={
+							<History currentUser={id} items={messages} itemsPerPage={25} />
+						}
+					/>
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</Router>
 		</>
 	) : (
 		<Login onNameSubmit={setId} />
